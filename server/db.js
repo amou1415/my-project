45df8c10723f4dee715c5dee3408c987e6a82bd9
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2021-12-16 12:38:11 +0800

    连接数据库

--- server/db.js
+++ server/db.js
@@ -1,1 +1,12 @@
 // 连接数据库
+const mysql = require('mysql');
+
+// 创建连接对象
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '02010898lxh',
+    port: '3306',
+    database: 'tt'
+});
+connection.connect();
