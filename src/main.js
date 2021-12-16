e727961ac3e6a32fbde2047031749b4633797c1d
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2021-12-16 12:14:09 +0800

    引入全局样式

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios';
+// 引入初始化样式
+import './assets/common.css'
 import App from './App.vue'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios;
 
 new Vue({
   render: h => h(App),
