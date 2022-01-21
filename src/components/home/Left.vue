47f04828ddaa9048745b240c32b1930f3fd91db5
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-01-21 14:07:49 +0800

    实现首页content组件左边游戏列表、icon入场动画。可优化部分：引入icon

--- src/components/home/Left.vue
+++ src/components/home/Left.vue
@@ -1,37 +1,37 @@
 <template>
   <div class="gameList fl">
-    <div class="item">
+    <div class="item" v-for="item in list" :key="item.id">
       <!-- 游戏标题内容 -->
       <div class="gameTitle clearfix">
         <div class="appLogo fl">
-          <router-link :to="{ path: '/details', query: { id: 1 } }">
-            <img src="" alt="" />
+          <router-link :to="{ path: '/details', query: { id: item.id } }">
+            <img :src="item.logo" :alt="item.name" />
           </router-link>
         </div>
-        <h3>
-          <router-link :to="{ path: '/details', query: { id: 1 } }">
-            猫
-          </router-link>
-        </h3>
+        <router-link :to="{ path: '/details', query: { id: item.id } }">
+          <h3 v-text="item.name"></h3>
+        </router-link>
         <p class="lab">
-          <label></label>
+          <label v-text="item.lab1"></label>
+          <label v-text="item.lab2"></label>
         </p>
       </div>
       <!-- 游戏描述、展示 -->
       <div class="gameImg">
-        <img
-          src="https://www.100game.cn/uploads/200601/1-200601103400Z6.jpg"
-          style="position: absolute"
-        />
+        <img :src="item.bigimg" style="position: absolute" />
         <div class="mask"></div>
-        <div class="img_msg">
-          <h3>在线撸猫开店，打造你的猫咪咖啡厅！</h3>
-          <div class="icons">
-            <p class="iconfont icon-dianzan">1</p>
-            <router-link :to="{ path: '/details', query: { id: 1 } }">
-              <p class="iconfont icon-icon_msg">2</p>
+        <div class="img_msg clearfix">
+          <h3 v-text="item.game_msg"></h3>
+          <div class="icons fl">
+            <a href="#" class="iconfont icon-dianzan">1</a>
+          </div>
+          <div class="icons fl">
+            <router-link :to="{ path: '/details', query: { id: item.id } }">
+              <span class="iconfont icon-icon_msg">2</span>
             </router-link>
-            <a href="#" class="iconfont icon-xiazai1">3</a>
+          </div>
+          <div class="icons fl">
+            <a :href="item.download" class="iconfont icon-xiazai1">3</a>
           </div>
         </div>
       </div>
@@ -40,33 +40,58 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      list: "",
+    };
+  },
+  mounted() {
+    this.getList();
+  },
+  methods: {
+    getList() {
+      let that_ = this;
+      axios
+        .post("/api/getGameList")
+        .then((res) => {
+          console.log(res.data.list, "res");
+          that_.list = res.data.list;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .gameList {
-  width: 820px;
-  margin-top: 20px;
+  width: 64.063vw;
+  margin-top: 1.563vw;
 
   .item {
     width: 100%;
-    height: 507px;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    padding: 20px;
-    box-shadow: -2px 1px 4px #cecccc;
+    height: 39.609vw;
+    margin-top: 1.563vw;
+    margin-bottom: 1.563vw;
+    padding: 1.563vw;
+    box-shadow: -0.156vw 0.078vw 4.006px #cecccc;
 
     .gameTitle {
-      width: 784px;
-      height: 64px;
+      width: 61.25vw;
+      height: 5vw;
 
       .appLogo {
-        width: 64px;
-        height: 64px;
+        width: 5vw;
+        height: 5vw;
         background-size: cover;
         background-position: 100% 100%;
         background-repeat: no-repeat;
-        margin-right: 20px;
+        margin-right: 1.563vw;
       }
       .title {
         height: 5vw;
@@ -75,6 +100,10 @@ export default {};
         color: #aaa;
         margin-top: 20px;
         font-size: 13px;
+
+        label {
+          margin-right: 7px;
+        }
       }
     }
 
@@ -110,20 +139,33 @@ export default {};
           transform: translateY(30px);
           transition: transform 0.4s;
         }
+        .icons {
+          position: absolute;
+          right: 40px;
+          bottom: 40px;
+          margin: 0 10px;
+          color: #333;
+        }
       }
-      .icon {
-        position: absolute;
-        right: 40px;
-        bottom: 40px;
-        margin: 0 10px;
-        color: #333;
-        transform: translateY(75px);
-        transition: transform 0.45s;
-      }
-      .icons :nth-child(1) {
+      .img_msg :nth-child(2) {
         color: #aaa;
         font-size: 22px;
-        transition: color 0.1s;
+        right: 70px;
+        transform: translateY(100px);
+        transition: transform 0.3s;
+      }
+      .img_msg :nth-child(3) {
+        color: #0aad0a;
+        font-size: 22px;
+        transform: translateY(120px);
+        transition: transform 0.4s;
+      }
+      .img_msg :nth-child(4) {
+        color: #ccb807;
+        font-size: 22px;
+        right: 10px;
+        transform: translateY(140px);
+        transition: transform 0.45s;
       }
     }
 
@@ -132,6 +174,9 @@ export default {};
         transform: translateY(0px);
       }
       .img_msg h3 {
+        transform: translateY(0px);
+      }
+      .icons {
         transform: translateY(0px);
       }
     }
