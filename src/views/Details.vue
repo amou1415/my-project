9725d745740d51d0c4b9bacd5f37e7a8aa49038b
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-01-21 14:05:05 +0800

    游戏详情页面，实现获取路由参数功能

---
+++ src/views/Details.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1 v-text="id"></h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        id:''
+    };
+  },
+  mounted() {
+    this.getMsg();
+  },
+  methods: {
+    getMsg() {
+      this.id = this.$route.query.id
+    },
+  },
+};
+</script>
+
+<style></style>
