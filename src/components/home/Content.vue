e9ee43888a365ac5d93a26d938a0271448d20806
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-01-21 14:04:25 +0800

    页面进一步组件化

--- src/components/home/Content.vue
+++ src/components/home/Content.vue
@@ -1,15 +1,26 @@
 <template>
   <main>
     <div class="content clearfix">
-
+        <Games></Games>
+        <NewGames></NewGames>
     </div>
   </main>
 </template>
 
 <script>
-export default {};
+import Games from '../home/Left'
+import NewGames from '../home/Right'
+export default {
+    components:{
+        Games,
+        NewGames
+    }
+};
 </script>
 
 <style lang="less" scoped>
-
+    .content {
+        width: 100vw;
+        padding: 0 6vw;
+    }
 </style>
