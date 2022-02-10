e641240034634c5898db24d8834525f606361f0e
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-02-10 17:39:03 +0800

    优化“首次登陆即注册”的登陆组件实现逻辑

--- server/index.js
+++ server/index.js
@@ -12,40 +12,64 @@ app.use(bodyParser.urlencoded({
 app.post('/login', (req, res) => {
     let data = req.body;
 
-    let sql = 'select * from user where u_name = ? and email = ?';
+    let sql = 'select * from user where email = ?';
     // 组装查询sql的s参数
     let sqlData = [
         data.username,
         data.password
     ]
 
-    mysqlbase.mysql_base(sql, sqlData, (results) => {
+    mysqlbase.mysql_base(sql, sqlData[1], (results) => {
         // 返回查询结果
         console.log(results[0], '结果');
+        /**
+         * 先验证邮箱是否存在
+         * 存在就验证用户名 匹配就抛出success 不匹配就抛出err
+         */
         if (results[0] != undefined) {
-            // 用户保存信息
-            res.send({
-                msg: 'true',
-                user: results
-            });
-        } else if (results[0] === undefined) {
-            let insertDate = {
-                u_name: data.username,
-                email: data.password,
+            if (sqlData[0] === results[0].u_name) {
+                res.send({
+                    msg: 'true',
+                    user: results
+                });
+            } else {
+                res.send({
+                    msg: 'false',
+                    error: '用户名或邮箱不匹配'
+                });
             }
-            mysqlbase.mysql_base("insert into user set ?", insertDate, (inser_results) => {
-                if (inser_results.affectedRows > 0) {
-                    mysqlbase.mysql_base('select * from user where u_name = ? and email = ?', sqlData, (inser_msg) => {
-                        res.send({
-                            msg: 'true',
-                            user: inser_msg
-                        });
-                    })
-                } else {
+        } else if (results[0] === undefined) {
+            /**
+             * 邮箱不存在就进入注册
+             * 先验证输入的用户名是否重复
+             * 不重复就注册，重复抛出错误
+             */
+            mysqlbase.mysql_base('select * from user where u_name = ?', sqlData[0], (name_res) => {
+                if (name_res[0] != undefined) {
                     res.send({
                         msg: 'false',
-                        user: inser_results
+                        error: '尊敬的新用户，该用户名已被使用'
                     });
+                } else {
+                    let insertDate = {
+                        u_name: data.username,
+                        email: data.password,
+                    }
+                    mysqlbase.mysql_base("insert into user set ?", insertDate, (inser_results) => {
+                        if (inser_results.affectedRows > 0) {
+                            mysqlbase.mysql_base('select * from user where u_name = ? and email = ?', sqlData, (inser_msg) => {
+                                res.send({
+                                    msg: 'true',
+                                    user: inser_msg
+                                });
+                            })
+                        } else {
+                            res.send({
+                                msg: 'false',
+                                user: inser_results
+                            });
+                        }
+                    })
                 }
             })
         } else {
@@ -103,21 +127,21 @@ app.post('/register', (req, res) => {
 })
 
 // 获取首页游戏列表
-app.post('/getGameList',(req, res) => {
+app.post('/getGameList', (req, res) => {
     let data = req.body;
     let sql;
-    console.log(data.id,'data');
-    if(data.id === undefined){
+    console.log(data.id, 'data');
+    if (data.id === undefined) {
         sql = 'SELECT * FROM game';
-        mysqlbase.mysql_base(sql,data,(game_msg)=>{
+        mysqlbase.mysql_base(sql, data, (game_msg) => {
             res.send({
                 msg: '成功',
                 list: game_msg
             });
         })
-    }else {
+    } else {
         sql = 'select * from game where id = ?';
-        mysqlbase.mysql_base(sql,[data.id],(res_)=>{
+        mysqlbase.mysql_base(sql, [data.id], (res_) => {
             console.log(res_);
             res.send({
                 msg: '成功',
@@ -125,7 +149,7 @@ app.post('/getGameList',(req, res) => {
             });
         })
     }
-    
+
 })
 
 
