511c4e5d626241f80c9eed1b1569f46ed3b1633f
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-02-11 15:07:52 +0800

    测试：获取首页路由跳转带来的参数，并通过pinia获取用户信息

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,20 +1,38 @@
 <template>
-  <h1 v-text="id"></h1>
+  <div>
+    <h1 v-text="id"></h1>
+    <h1>{{ msg }}</h1>
+  </div>
 </template>
 
-<script>
+<script setup>
+import { mapStores } from "pinia";
+import { useUserStore } from "../store/user";
+import { computed } from "@vue/composition-api";
+
+const mainStore = useUserStore();
+
 export default {
   data() {
     return {
-        id:''
+      id: "",
+      msg: "",
     };
+  },
+  computed: {
+    ...mapStores(mainStore),
+  },
+  watch: {
+    
   },
   mounted() {
     this.getMsg();
   },
   methods: {
     getMsg() {
-      this.id = this.$route.query.id
+      this.id = this.$route.query.id;
+      this.msg = computed(() => mainStore.userMsg);
+      console.log(this.msg, "msg");
     },
   },
 };
