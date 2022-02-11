e60224a692c9a761baed2fcd25d464a68512ba5a
amou1415 <5662129+xinhaol@user.noreply.gitee.com>
2022-02-11 15:04:11 +0800

    添加注释，删除弃用代码

--- server/index.js
+++ server/index.js
@@ -79,58 +79,16 @@ app.post('/login', (req, res) => {
         }
     })
 })
-// 注册
-app.post('/register', (req, res) => {
-    // 1、路径请求第一步：现货区数据
-    let data = req.body;
-    // console.log(11);
-    // 2、路径请求第二步：进行数据处理
-    // *是否能注册，首先要判断这个人是否已经注册
-    let username = data.username;
-    let sql = "select count(1) cn from user where u_name=?"
-    mysqlbase.mysql_base(sql, [username], (results) => {
-        console.log(results);
-        console.log(results[0].cn);
-        if (results[0].cn > 0) {
-            res.send({
-                msg: '该姓名已经注册过',
-                flag: 'false'
-            });
-            // res.send('register.html',{msg:'该姓名已经注册过',flag:'false'});
-            // return; // 把程序打断掉
-        } else {
-            console.log('程序继续执行');
-
-            // *没有注册过的，插入数据
-            sql = "insert into user set ?"
-            let insertDate = {
-                id: data.id,
-                username: data.username,
-                password: data.password,
-            }
-            mysqlbase.mysql_base(sql, insertDate, (results) => {
-                console.log(results);
-                if (results.affectedRows > 0) {
-                    // res.send({ msg: "注册成功", falg: 'true' });
-                    res.send({
-                        msg: 'true'
-                    });
-                } else {
-                    res.send({
-                        falg: 'false'
-                    });
-                }
-
-            })
-        }
-    })
-})
 
 // 获取首页游戏列表
 app.post('/getGameList', (req, res) => {
     let data = req.body;
     let sql;
     console.log(data.id, 'data');
+    /**
+     * 请求参数带id就返回指定游戏id
+     * 不带参数就返回游戏列表
+     */
     if (data.id === undefined) {
         sql = 'SELECT * FROM game';
         mysqlbase.mysql_base(sql, data, (game_msg) => {
